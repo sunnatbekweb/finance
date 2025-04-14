cf2d3f6e78a9237c2f54abcef54e9486a30cd177
sunnatbekweb <sunnatbekkadamov@gmail.com>
2025-04-14 15:43:54 +0500

    created welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <></>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="max-w-[900px] mx-auto">
+        <h1 className="text-[40px] font-semibold text-center text-[#1E1E1E] mb-6">
+          <span className="font-normal">Welcome to the</span> <br />
+          Financial Management Dashboard
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
